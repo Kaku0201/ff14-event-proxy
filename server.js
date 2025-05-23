c8b599a5250e3dec1e0ef19fdf770f971919b152
Kaku0201 <kakurim0201@gmail.com>
2025-05-23 17:08:14 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js - Render 호환 puppeteer-core 버전
 import express from 'express';
 import puppeteer from 'puppeteer-core';
-import { executablePath } from 'puppeteer';
+import { executablePath } from 'puppeteer-core';
 import cors from 'cors';
 
 const app = express();
